3882fca7d6fa398c94796185b3fbd321518d3d7b
Sabrina <goncalvessabrina74@gmail.com>
2023-09-05 21:40:04 -0300

    ap02(input-output): variaveis declaradas

---
+++ Operacoes.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Operacoes{
+    public static void main(String [] args){
+         //declaração de variaveis 
+        double num1;
+        double num2;
+        double resultado;     
+        Scanner leitor = new Scanner(System.in);  
+
+        //entrada
+        
+
+        //processamento
+        
+
+        //saida
+
+
+    }
+}
